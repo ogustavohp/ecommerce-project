9a765643aa6a2853a2bde23e0e4aef0df0cbb80e
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-10-04 17:15:22 -0300

    add turbo repo and create nest server

---
+++ apps/web/src/components/product/product-list.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { products } from '@/core'
+import { ProductItem } from './product-item'
+import { ProductNotFound } from './product-not-found'
+
+export function ProductList() {
+  if (!products.length) {
+    return <ProductNotFound />
+  }
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      {products.map((product, i) => (
+        <ProductItem key={product.id + i} product={product} />
+      ))}
+    </div>
+  )
+}
